cb6a3f15e0d5d323f118db1df58e9912acc83b35
agung saputra <agung.dni19@gmail.com>
2025-07-19 22:19:56 +0700

    ignore koneksi file

--- bawaan_koneksi/konek2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$konek2 = mysqli_connect("localhost", "root", "root", "pengaduan_masyarakat");
-
-if (!$konek2) {
-    die("Koneksi gagal: " . mysqli_connect_error());
-}
--- bawaan_koneksi/konek3.php
+++
@@ -1,5 +0,0 @@
-<?php
-$konek3 = new mysqli("localhost", "root", "root", "sidkdd");
-if ($konek3->connect_error) {
-    die("Koneksi 3 gagal: " . $konek3->connect_error);
-}
