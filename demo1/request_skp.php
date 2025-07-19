a81549c1f86638c1606b1783cbe1877942022762
agung saputra <agung.dni19@gmail.com>
2025-07-19 22:52:08 +0700

    update path sebagian

--- demo1/request_skp.php
+++ demo1/request_skp.php
@@ -1,102 +1,102 @@
-<?php include '../koneksi/konek1.php';?>
+<?php include '../koneksi/konek1.php'; ?>
 <link href="css/sweetalert.css" rel="stylesheet" type="text/css">
 <script src="js/jquery-2.1.3.min.js"></script>
-<script src="js/sweetalert.min.js"></script>  
+<script src="js/sweetalert.min.js"></script>
 <?php
-	$tampil_nik = "SELECT * FROM data_user WHERE nik=$_SESSION[nik]";
-	$query = mysqli_query($konek1,$tampil_nik);
-	$data = mysqli_fetch_array($query,MYSQLI_BOTH);
-	$nik = $data['nik'];
-	$nama = $data['nama'];
+$tampil_nik = "SELECT * FROM data_user WHERE nik=$_SESSION[nik]";
+$query = mysqli_query($konek1, $tampil_nik);
+$data = mysqli_fetch_array($query, MYSQLI_BOTH);
+$nik = $data['nik'];
+$nama = $data['nama'];
 ?>
 <div class="page-inner">
-					<div class="row">
-						<div class="col-md-12">	
-						<form method="POST" enctype="multipart/form-data">
-							<div class="card">
-								<div class="card-header">
-									<div class="card-title">FORM TAMBAH REQUEST SURAT IZIN USAHA</div>
+	<div class="row">
+		<div class="col-md-12">
+			<form method="POST" enctype="multipart/form-data">
+				<div class="card">
+					<div class="card-header">
+						<div class="card-title">FORM TAMBAH REQUEST SURAT IZIN USAHA</div>
+					</div>
+					<div class="card-body">
+						<div class="row">
+							<div class="col-md-6 col-lg-6">
+								<div class="form-group">
+									<label>NIK</label>
+									<input type="text" class="form-control" value="<?= $nik . ' - ' . $nama; ?>" readonly>
 								</div>
-								<div class="card-body">
-									<div class="row">
-											<div class="col-md-6 col-lg-6">
-												<div class="form-group">
-													<label>NIK</label>
-													<input type="text" class="form-control" value="<?= $nik.' - '.$nama;?>" readonly>
-												</div>
-												<div class="form-group">
-													<input type="hidden" name="nik" class="form-control" value="<?= $nik;?>" readonly>
-												</div>
-												<div class="form-group">
-													<label>Nama Usaha</label>
-													<input type="text" name="nama_usaha" class="form-control" placeholder="Nama Usaha Anda.." autofocus>
-												</div>
-											<div class="form-group">
-													<label>Jenis Usaha</label>
-													<input type="text" name="jenis_usaha" class="form-control" placeholder="Jenis Usaha Anda.." autofocus>
-												</div>
-											</div>
-											<div class="col-md-6 col-lg-6">
-												<div class="form-group">
-													<label>Alamat Usaha</label>
-													<input type="text" name="alamat_usaha" class="form-control" placeholder="Alamat Usaha Anda.." autofocus>
-												</div>	
-												<div class="form-group">
-													<label>Scan KTP</label>
-													<input type="file" name="ktp" class="form-control" size="37" required>
-												</div>
-												<div class="form-group">
-													<label>Scan KK</label>
-													<input type="file" name="kk" class="form-control" size="37" required>
-												</div>
-												<div class="form-group">
-    <label>Scan Surat Pengantar RT/RW</label>
-    <input type="file" name="pengantar_rt_rw" class="form-control" required>
-</div>
-
-											</div>
-									</div>
+								<div class="form-group">
+									<input type="hidden" name="nik" class="form-control" value="<?= $nik; ?>" readonly>
 								</div>
-								<div class="card-action">
-									<button name="kirim" class="btn btn-success">Kirim</button>
-									<a href="?halaman=beranda" class="btn btn-default">Batal</a>
+								<div class="form-group">
+									<label>Nama Usaha</label>
+									<input type="text" name="nama_usaha" class="form-control" placeholder="Nama Usaha Anda.." autofocus>
+								</div>
+								<div class="form-group">
+									<label>Jenis Usaha</label>
+									<input type="text" name="jenis_usaha" class="form-control" placeholder="Jenis Usaha Anda.." autofocus>
 								</div>
 							</div>
+							<div class="col-md-6 col-lg-6">
+								<div class="form-group">
+									<label>Alamat Usaha</label>
+									<input type="text" name="alamat_usaha" class="form-control" placeholder="Alamat Usaha Anda.." autofocus>
+								</div>
+								<div class="form-group">
+									<label>Scan KTP</label>
+									<input type="file" name="ktp" class="form-control" size="37" required>
+								</div>
+								<div class="form-group">
+									<label>Scan KK</label>
+									<input type="file" name="kk" class="form-control" size="37" required>
+								</div>
+								<div class="form-group">
+									<label>Scan Surat Pengantar RT/RW</label>
+									<input type="file" name="pengantar_rt_rw" class="form-control" required>
+								</div>
+
+							</div>
 						</div>
-						</form>
 					</div>
+					<div class="card-action">
+						<button name="kirim" class="btn btn-success">Kirim</button>
+						<a href="?halaman=beranda" class="btn btn-default">Batal</a>
+					</div>
+				</div>
+		</div>
+		</form>
+	</div>
 </div>
 
 <?php
-if(isset($_POST['kirim'])){
+if (isset($_POST['kirim'])) {
 	$nik = $_POST['nik'];
 	$nama_ktp = isset($_FILES['ktp']);
-	$file_ktp = $_POST['nik']."_".".jpg";
+	$file_ktp = $_POST['nik'] . "_" . ".jpg";
 	$nama_kk = isset($_FILES['kk']);
-    $file_kk = $_POST['nik']."_".".jpg";
+	$file_kk = $_POST['nik'] . "_" . ".jpg";
 	$nama_usaha = $_POST['nama_usaha'];
 	$jenis_usaha = $_POST['jenis_usaha'];
 	$alamat_usaha = $_POST['alamat_usaha'];
 	$nama_pengantar = isset($_FILES['pengantar_rt_rw']);
-$file_pengantar = $_POST['nik']."_pengantar_rt_rw.jpg";
+	$file_pengantar = $_POST['nik'] . "_pengantar_rt_rw.jpg";
 
-$sql = "INSERT INTO data_request_skp 
+	$sql = "INSERT INTO data_request_skp 
         (nik, scan_ktp, scan_kk, scan_pengantar_rt_rw, nama_usaha, jenis_usaha, alamat_usaha) 
         VALUES 
         ('$nik', '$file_ktp', '$file_kk', '$file_pengantar', '$nama_usaha', '$jenis_usaha', '$alamat_usaha')";
-$query = mysqli_query($konek1, $sql) or die(mysqli_error($konek1));
+	$query = mysqli_query($konek1, $sql) or die(mysqli_error($konek1));
 
 
-	if($query){
-		copy($_FILES['ktp']['tmp_name'],"../dataFoto/scan_ktp/".$file_ktp);
-		copy($_FILES['kk']['tmp_name'],"../dataFoto/scan_kk/".$file_kk);
-		copy($_FILES['pengantar_rt_rw']['tmp_name'], "../dataFoto/scan_pengantar_rt_rw/".$file_pengantar);
-		echo "<script language='javascript'>swal('Selamat...', 'Kirim Berhasil', 'success');</script>" ;
+	if ($query) {
+		copy($_FILES['ktp']['tmp_name'], "../dataFoto/scan_ktp/" . $file_ktp);
+		copy($_FILES['kk']['tmp_name'], "../dataFoto/scan_kk/" . $file_kk);
+		copy($_FILES['pengantar_rt_rw']['tmp_name'], "../dataFoto/scan_pengantar_rt_rw/" . $file_pengantar);
+		echo "<script language='javascript'>swal('Selamat...', 'Kirim Berhasil', 'success');</script>";
 		echo '<meta http-equiv="refresh" content="3; url=?halaman=tampil_status">';
-	  }else{
-		echo "<script language='javascript'>swal('Gagal...', 'Kirim Gagal', 'error');</script>" ;
+	} else {
+		echo "<script language='javascript'>swal('Gagal...', 'Kirim Gagal', 'error');</script>";
 		echo '<meta http-equiv="refresh" content="3; url=?halaman=request_skp">';
-	  }
+	}
 }
-	
+
 ?>
