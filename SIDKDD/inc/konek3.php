d66e0a29b512ecd216d11d4fc852962de0e1040a
agung saputra <agung.dni19@gmail.com>
2025-07-21 18:43:00 +0700

    database dan perbaikan cetak header

--- SIDKDD/inc/konek3.php
+++ SIDKDD/inc/konek3.php
@@ -1,6 +1,5 @@
 <?php
-$konek3 = new mysqli("localhost", "root", "", "sidkdd");
+$konek3 = new mysqli("localhost", "vrrs2386_bogorejo", "bogorejo2025@#*", "vrrs2386_sidkdd");
 if ($konek3->connect_error) {
     die("Koneksi 3 gagal: " . $konek3->connect_error);
 }
-?>
